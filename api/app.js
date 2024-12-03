e42d877a7857dcfb5d5bacf49a375bd219ba6c89
Alireza <alirezasaadat056@gmail.com>
2024-12-03 11:38:22 +0330

    project structure

--- api/app.js
+++ api/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
+const cors = require("cors");
 const v1 = require("./src/routers/router");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(v1);
 
