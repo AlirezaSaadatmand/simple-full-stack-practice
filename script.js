c6708509f1248565cdb9d613b6b4855d98bab590
Alireza <alirezasaadat056@gmail.com>
2024-12-03 18:13:26 +0330

    connecting frontend and backend

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
 function getRequest(url) {
     fetch(url, {
+        method: "GET",
         headers: {
-            Accept: "application/json",
+            "Content-Type": "application/json",
         },
     })
-        .then((response) => response.json())
-        .then((text) => {
-            return text;
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            const taskList = document.getElementById("taskList");
+            taskList.innerHTML = "";
+            data.tasks.forEach((task) => {
+                const newTask = document.createElement("li");
+                const spanText = document.createElement("span");
+                spanText.innerHTML = task.task;
+                spanText.style.width = "130px";
+
+                const btnEdit = document.createElement("button");
+                btnEdit.textContent = "ویرایش";
+                btnEdit.addEventListener("click", function () {
+                    editTodoList(newTask, spanText, taskInput);
+                });
+
+                const btnDelete = document.createElement("button");
+                btnDelete.textContent = "حذف";
+                btnDelete.addEventListener("click", function () {
+                    deleteRequest("http://localhost:3000/api/tasks/" + task.id, newTask);
+                });
+
+                newTask.addEventListener("click", function () {
+                    newTask.classList.toggle("completed");
+                });
+
+                taskList.appendChild(newTask);
+                newTask.appendChild(spanText);
+                newTask.appendChild(btnEdit);
+                newTask.appendChild(btnDelete);
+            });
         });
 }
+
+getRequest("http://localhost:3000/api/tasks");
 
 function postRequest(url, body) {
     fetch(url, {
@@ -25,11 +61,27 @@ function postRequest(url, body) {
             return response.json();
         })
         .then((data) => {
+            getRequest("http://localhost:3000/api/tasks");
             return data;
         });
 }
 
-function deleteRequest(url) {
+document.getElementById("addTaskBtn").addEventListener("click", () => {
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        const body = {
+            task: taskText,
+            taskStatus: "undone",
+        };
+        postRequest("http://localhost:3000/api/tasks", body);
+        taskInput.value = "";
+        taskInput.focus();
+    }
+});
+
+function deleteRequest(url, newTask) {
     fetch(url, {
         method: "DELETE",
         headers: {
@@ -45,71 +97,33 @@ function deleteRequest(url) {
         .then((data) => {
             return data;
         });
-}
 
-function patchRequest(url, body) {
-    fetch(url, {
-        method: "PATCH",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-    })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then((data) => console.log("PUT Response:", data))
-        .catch((error) => console.error("Error:", error));
-}
-
-document.getElementById("addTaskBtn").addEventListener("click", function addTodoList(newTask) {
-    const taskInput = document.getElementById("taskInput");
-    const taskText = taskInput.value.trim();
-
-    if (taskText !== "") {
-        const taskList = document.getElementById("taskList");
-        const newTask = document.createElement("li");
-        const spanText = document.createElement("span");
-        spanText.innerHTML = taskText;
-        spanText.style.width = "130px";
-
-        const btnEdit = document.createElement("button");
-        btnEdit.textContent = "ویرایش";
-        btnEdit.addEventListener("click", function () {
-            editTodoList(newTask, spanText, taskInput);
-        });
-
-        const btnDelete = document.createElement("button");
-        btnDelete.textContent = "حذف";
-        btnDelete.addEventListener("click", function () {
-            deleteTodoList(newTask);
-        });
-
-        newTask.addEventListener("click", function () {
-            newTask.classList.toggle("completed");
-        });
-
-        taskList.appendChild(newTask);
-        newTask.appendChild(spanText);
-        newTask.appendChild(btnEdit);
-        newTask.appendChild(btnDelete);
-        taskInput.value = "";
-        taskInput.focus();
-    }
-});
-
-function deleteTodoList(newTask) {
     newTask.remove();
 }
 
-function editTodoList(newTask, spanText, taskInput) {
-    taskInput.value = spanText.innerHTML;
-    taskInput.focus();
-    document.getElementById("addTaskBtn").addEventListener("click", function () {
-        addTodoList(newTask);
-    });
-    newTask.remove();
-}
+// function patchRequest(url, body) {
+//     fetch(url, {
+//         method: "PATCH",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(body),
+//     })
+//         .then((response) => {
+//             if (!response.ok) {
+//                 throw new Error(`HTTP error! Status: ${response.status}`);
+//             }
+//             return response.json();
+//         })
+//         .then((data) => console.log("PUT Response:", data))
+//         .catch((error) => console.error("Error:", error));
+// }
+
+// // function editTodoList(newTask, spanText, taskInput) {
+// //     taskInput.value = spanText.innerHTML;
+// //     taskInput.focus();
+// //     document.getElementById("addTaskBtn").addEventListener("click", function () {
+// //         addTodoList(newTask);
+// //     });
+// //     newTask.remove();
+// // }
